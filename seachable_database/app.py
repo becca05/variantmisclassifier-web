c56dd9120a02106cbcaf045172c57a93d3995bb4
becca05 <73406653+becca05@users.noreply.github.com>
2022-11-05 08:40:20 -0400

    2

--- seachable_database/app.py
+++ seachable_database/app.py
@@ -4,7 +4,7 @@ from flask import render_template, flash, redirect, url_for, Flask, request
 app = Flask(__name__)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
 
 @app.route('/')
 def index():
